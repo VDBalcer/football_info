3ec213a43a1f31256dd85d6a0042feb75ece77bf
Vadim <VDBalcer@gmail.com>
2021-02-20 20:50:11 +0300

    Create and register Comment model

--- football_info/player_info/admin.py
+++ football_info/player_info/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Player
+from .models import Player, Comment
 
 
-admin.site.register(Player)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'pub_date', 'text')
+    search_fields = ('author',)
+    list_filter = ('pub_date',)
+    empty_value_display = "-Гость-"
 
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth')
+    search_fields = ('name',)
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Player, PlayerAdmin)
--- football_info/player_info/models.py
+++ football_info/player_info/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class Player(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     birth = models.DateField()
     info = models.TextField()
     photo = models.FileField(blank=True, null=True)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return self.name
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=25, blank=True, null=True)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
