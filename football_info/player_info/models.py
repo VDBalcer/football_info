1504c7432fe7ae770e5e8beedc20fbbfb0e46e31
Vadim <VDBalcer@gmail.com>
2021-02-22 21:39:24 +0300

    Add styles and templates

--- football_info/player_info/models.py
+++ football_info/player_info/models.py
@@ -5,14 +5,14 @@ class Player(models.Model):
     name = models.CharField(max_length=50)
     birth = models.DateField()
     info = models.TextField()
-    photo = models.FileField(blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Comment(models.Model):
-    author = models.CharField(max_length=25)
+    author = models.CharField(max_length=25, default='-Гость-')
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
--- football_info/player_info/urls.py
+++ football_info/player_info/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('players/', views.index),
+    path('players/', views.index, name = 'Main'),
     path('players/<int:id>', views.player_page),
 ]
