50199dfd557bcd3b42a909d92f98ad92ed6eba05
Vadim <VDBalcer@gmail.com>
2021-02-22 21:43:18 +0300

    Redirect empty url; add requirements.txt

--- football_info/football_info/urls.py
+++ football_info/football_info/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('player_info.urls')),
+    path('', RedirectView.as_view(url='/players', permanent=True)),
 ]
