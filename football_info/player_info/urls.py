fdc071e711f63f764507ef0dbc2152e46375c329
Vadim <VDBalcer@gmail.com>
2021-02-22 21:41:53 +0300

    Working with statics

--- football_info/player_info/urls.py
+++ football_info/player_info/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('players/', views.index, name = 'Main'),
+    path('players/', views.index, name='Main'),
     path('players/<int:id>', views.player_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
